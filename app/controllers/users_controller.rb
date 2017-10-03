fb4ebdbd830de1db5d896986c31f860617b5834c
Larry Sanger <yo.larrysanger@gmail.com>
2017-10-03 03:23:19 +0000

    user edit, update, index, and destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
   def new
     @user = User.new
+  end
+  
+  def index
+    @users = User.paginate(page: params[:page])
   end
   
   def show
@@ -13,9 +20,29 @@ class UsersController < ApplicationController
       log_in @user
       remember @user
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      redirect_back_or @user
     else
       render 'new'
+    end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "Another one bites the dust!"
+    redirect_to users_url
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      render 'edit'
     end
   end
 
@@ -25,5 +52,28 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, 
                                    :password_confirmation)
     end
+    
+    # Before filters
+    
+    # Confirms a user is in fact logged in.
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+    
+    # Confirms a user is in fact the correct user.
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    # Confirms that a user is an admin user.
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+  
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name:    "Filby Federson",
+             email:   "feder@filby.com",
+             password:              "Password9",
+             password_confirmation: "Password9")
+
+emails = []
+99.times do |n| 
+  test = Faker::Internet.email
+  emails.include?(test) ? redo : emails[n] = test
+end
+
+99.times do |n|
+  name = Faker::Name.name
+  email = emails[n]
+  password = "Password9"
+  User.create!(name: name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
