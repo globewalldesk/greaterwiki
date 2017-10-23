ab77074468cea4540fd7231b0812fc798a02ba51
globewalldesk <yo.larrysanger@gmail.com>
2017-10-22 23:40:26 -0400

    minor updates for minor launch

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-# Greater Wiki
+# StartThis.org
 
-This app will enable a community—or different competing communities—to link to
-as well as rate/rank encyclopedia articles, creating the world's first 
-repository of the best encyclopedia articles on everything everywhere.
+Wait for it!
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   # Other action_mailer config, per Hartl.
   config.action_mailer.delivery_method = :test
-  host = 'greaterwiki-lsanger.c9users.io'
+  host = 'startthis-lsanger.c9users.io'
   config.action_mailer.default_url_options = { host: host }
 
   # Print deprecation notices to the Rails logger.
